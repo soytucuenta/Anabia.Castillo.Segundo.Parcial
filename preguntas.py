1f43e9cc042c084090ab6d055b6d5687225d4058
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-06-13 17:19:08 -0300

    ADD: Preguntas

---
+++ preguntas.py
@@ -0,0 +1,155 @@
+nivel_1 = {
+    "pregunta_1":{
+        "pregunta": "¿Qué elemento químico tiene el símbolo 'O'?",
+        "opciones": [
+        "Osmio", 
+        "Oro", 
+        "Oxígeno", 
+        "Oganesón"
+        ],
+        "correcta": "Oxígeno"
+    },
+    "pregunta_2":{
+        "pregunta": "¿Qué planeta es conocido como 'el planeta rojo'?",
+        "opciones": [
+        "Venus", 
+        "Júpiter", 
+        "Marte", 
+        "Saturno"],
+        "correcta": "Marte"
+    },
+    "pregunta_3":{
+        "pregunta": "¿Cuál es el río más largo del mundo?",
+        "opciones": [ 
+        "Nilo", 
+        "Misisipi", 
+        "Yangtsé",
+        "Amazonas"], 
+        "correcta": "Amazonas"
+    },
+    "pregunta_4":{
+        "pregunta": "¿Quién pintó 'La Mona Lisa'?",
+        "opciones": [
+        "Pablo Picasso", 
+        "Leonardo da Vinci", 
+        "Vincent van Gogh", 
+        "Salvador Dalí"],
+        "correcta": "Leonardo da Vinci"
+    },
+    "pregunta_5":{
+        "pregunta": "¿En qué año llegó el hombre a la luna?",
+        "opciones": [
+        "1969",
+        "1955", 
+        "1975", 
+        "1989"
+        ],
+        "correcta": "1969"
+    }
+}
+
+nivel_2 = {
+    "pregunta_1":{
+        "pregunta": "¿Qué imperio fue gobernado por Julio César?",
+        "opciones": [
+        "Imperio británico"
+        "Imperio mongol", 
+        "Imperio romano", 
+        "Imperio otomano", 
+        ],
+        "correcta": "Imperio romano"
+    },
+    "pregunta_2":{
+        "pregunta": "¿En qué obra literaria aparece el personaje 'Jay Gatsby'?",
+        "opciones": [
+        "Moby Dick"
+        "Matar a un ruiseñor", 
+        "El gran Gatsby", 
+        "1984", 
+        ],
+        "correcta": "El gran Gatsby"
+    },
+    "pregunta_3":{
+        "pregunta": "¿Qué artista musical es conocido como 'El Rey del Pop'?",
+        "opciones": [ 
+        "Freddie Mercury", 
+        "Madonna"
+        "Michael Jackson", 
+        "Elvis Presley", 
+        ], 
+        "correcta": "Michael Jackson"
+    },
+    "pregunta_4":{
+        "pregunta": "¿Cuál es el metal más abundante en la corteza terrestre?",
+        "opciones": [
+        "Oro"
+        "Hierro", 
+        "Aluminio", 
+        "Cobre", 
+        ],
+        "correcta": "Aluminio"
+    },
+    "pregunta_5":{
+        "pregunta": "¿Qué famoso científico formuló la teoría de la relatividad?",
+        "opciones": [
+        "Isaac Newton", 
+        "Stephen Hawking", 
+        "Nikola Tesla"
+        "Albert Einstein", 
+        ],
+        "correcta": "Albert Einstein"
+    }
+}
+
+nivel_3 = {
+    "pregunta_1":{
+        "pregunta": "¿Qué poeta escribió 'El cuervo'?",
+        "opciones": [
+        "William Shakespeare", 
+        "Pablo Neruda", 
+        "Edgar Allan Poe", 
+        "Mario Benedetti"
+        ],
+        "correcta": "Edgar Allan Poe"
+    },
+    "pregunta_2":{
+        "pregunta": "¿En qué país se originó el sushi?",
+        "opciones": [
+        "Tailandia", 
+        "China", 
+        "Japón", 
+        "Corea del Sur", 
+        ],
+        "correcta": "Francia"
+    },
+    "pregunta_3":{
+        "pregunta": "¿Qué filósofo dijo 'Pienso, luego existo'?",
+        "opciones": [ 
+        "Friedrich Nietzsche"
+        "Platón", 
+        "Aristóteles", 
+        "René Descartes", 
+        ], 
+        "correcta": "René Descartes"
+    },
+    "pregunta_4":{
+        "pregunta": "¿En qué año se disolvió la Unión Soviética?",
+        "opciones": [
+        "1975"
+        "1989", 
+        "1991", 
+        "1993", 
+        ],
+        "correcta": "1991"
+    },
+    "pregunta_5":{
+        "pregunta": "¿Qué lengua romance se habla en Rumania?",
+        "opciones": [
+        "Louis Pasteur", 
+        "Marie Curie", 
+        "Gregor Mendel"
+        "Alexander Fleming", 
+        ],
+        "correcta": "Alexander Fleming"
+    }
+}
