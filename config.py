1414e3079d8327d75280e86a62be2889efbb03be
Francisco Javier Anabia <franciscojavier.anabia@gmail.com>
2025-07-02 02:12:51 -0300

    datos de usuarios en csv agregados(ganancias, contador de partidas,etc)

---
+++ config.py
@@ -0,0 +1,19 @@
+configuracion_default = {
+    "dificultad": "media",
+    "tiempo": 60,
+    "dinero_inicial": 1000000,
+    "neurodivergencia": False,
+    "categoria": "todas",
+}
+
+dificultades = {
+    "facil": {
+        "pool_preguntas": "facil"
+    },
+    "media": {
+        "pool_preguntas": "media"
+    },
+    "dificil": {
+        "pool_preguntas": "dificil"
+    }
+}
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from preguntas import *
 from logica_preguntas import *
 from usuarios import *
 from leer_escribir_archivos import *
+from funciones_genericas import *
 # Trampa
 cheats = True #pasa por parametro a gameplay y despues a la de mostrar pregunta y dice el numero de opcion de la correcta
 #
@@ -14,10 +15,14 @@ usuarios = cargar_usuarios('usuarios.csv') #carga los usuarios desde el CSV
 info_usuario = seleccion_usuario(usuarios)
 opcion_menu = int(input(menu))
 
-while opcion_menu != 7:
+while opcion_menu != 6:
     match opcion_menu:
         case 1:
-            gameplay(1000000, 60, cheats)
+            #contador participaciones
+            operar_en_clave_especifica(usuarios, 'nombre', info_usuario, incrementar_clave_especifica, 'participaciones')
+            dinero = gameplay(1000000, 60, cheats)
+            #suma ganancias
+            operar_en_clave_especifica(usuarios, 'nombre', info_usuario, sumar_en_clave, 'ganancias',dato_entrante=dinero)
         case 2:
             print(instrucciones)
         case 3:
@@ -25,11 +30,13 @@ while opcion_menu != 7:
         case 4:#muestra el listado de usuarios
             for usuario in usuarios:
                 mostrar_datos_usuario(usuario, "\nMostrando informacion de todos los usuarios: \n")
-        case 6:
+        case 5:
             pass
 
     # volver a preguntar por el menu
     opcion_menu = int(input(menu))
+print(info_usuario)
+print(usuarios)
 #guardado de datos de usuarios
 escribir_csv_usuarios(usuarios, 'usuarios.csv')
 print("guardando puntajes... \n")
