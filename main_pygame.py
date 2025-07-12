bea07a3e713cddc309f5ad2324f32b19ba9be013
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-07-11 22:01:42 -0300

    UPDATE: main_pygame Ordenado

--- main_pygame.py
+++ main_pygame.py
@@ -1,40 +1,42 @@
 import pygame
 from funciones_pygame import *
-
-pygame.init()
 from prints import *
 
-# música 
-# pygame.mixer.init()
-# musica_fondo = pygame.mixer.Sound('assets/cancion_fondo.mp3')
-# musica_fondo.set_volume(0.50)
-# musica_fondo.play(-1)
+pygame.init()
+
+#PROPIEDADES
+ANCHO_VENTANA = 640
+ALTO_VENTANA = 360
+VENTANA = pygame.display.set_mode((ANCHO_VENTANA,ALTO_VENTANA))
+CENTRO_PANTALLA = (ANCHO_VENTANA // 2, ALTO_VENTANA // 2)
 POSICION_BOTON_INICIAR = (40,395)
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
-#####ventana##########
-ANCHO_VENTANA = 640
-ALTO_VENTANA = 360
-CENTRO_PANTALLA = (ANCHO_VENTANA // 2, ALTO_VENTANA // 2)
-VENTANA = pygame.display.set_mode((ANCHO_VENTANA,ALTO_VENTANA))
 
-pygame.display.set_caption("Salven el Millon")#-------------------titulo de la ventana
-icono = pygame.image.load("assets/ver_guita.png")#------------------ icono de la ventana
-pygame.display.set_icon(icono)#------------------ icono de la ventana
-# imagén de fondo
+#VENTANA PROPIEDADES
+pygame.display.set_caption("SALVEN EL MILLÓN")
+icono = pygame.image.load("assets/ver_guita.png")
+pygame.display.set_icon(icono)
 
+#IMÁGENES
 fondo = pygame.image.load('assets/susan_fondo_bienvenidad.png')
 fondo_2 = pygame.image.load('assets/susana_fondo_jugando.png')
-
 fondo = pygame.transform.scale(fondo, (ANCHO_VENTANA, ALTO_VENTANA))
 fondo_jugando = pygame.transform.scale(fondo_2, (ANCHO_VENTANA, ALTO_VENTANA))
 
+#FUENTES
 texto_prueba = menu
 fuente_importada = pygame.font.Font('assets/PokemonGb-RAeo.ttf',24) ####### NO RECONOCE LAS FUENTES IMPORTADAS EN BOTONES, INVESTIGAR 
 superficie_texto = fuente_importada.render(texto_prueba, True, BLANCO)
 posicion_texto = superficie_texto.get_rect(center=POSICION_BOTON_INICIAR)
-#################################
-# BOTONES
+
+#MÚSICA
+# pygame.mixer.init()
+# musica_fondo = pygame.mixer.Sound('assets/cancion_fondo.mp3')
+# musica_fondo.set_volume(0.50)
+# musica_fondo.play(-1)
+
+#BOTONES
 boton_iniciar = crear_boton((200, 50), POSICION_BOTON_INICIAR, VENTANA, color_texto="Black", color_fondo="Yellow", texto="Iniciar", fuente=('assets/PokemonGb-RAeo.ttf', 24))
 boton_estadisticas = crear_boton((200, 50), (40, 470), VENTANA, color_texto="Black", color_fondo="Yellow", texto="Estadisticas", fuente=('assets/PokemonGb-RAeo.ttf', 24))
 boton_configuracion = crear_boton((200, 50), (40, 540), VENTANA, color_texto="Black", color_fondo="Yellow", texto="Configuracion", fuente=('assets/PokemonGb-RAeo.ttf', 24))
@@ -59,9 +61,10 @@ estado_del_programa = {####!!!!!!!!!! ACORDARSE DE BAJAR LAS BANDERAS CUANDO SE 
 }
 #########
 
-VENTANA.blit(fondo, (0, 0))#FONDO
 
 while estado_del_programa['salir'] == False:
+    VENTANA.blit(fondo, (0, 0))#FONDO
+
     for evento in pygame.event.get():#gestor de eventos
         print(evento)
         ###############################################
@@ -70,7 +73,7 @@ while estado_del_programa['salir'] == False:
             if estado_del_programa["menu_principal"]:#buscar como modularizar esto
                 buscar_boton_presionado(lista_de_botones_menu_principal, evento)
             elif estado_del_programa["partida_iniciada"]:
-                VENTANA.blit(fondo_jugando, (0, 0))#FONDO
+                VENTANA.blit(fondo_jugando, (0, 0)) #FONDO_JUGANDO
             elif estado_del_programa["configuracion"]:
                 pass
             elif estado_del_programa["estadisticas"]:
@@ -82,7 +85,6 @@ while estado_del_programa['salir'] == False:
     #############################
 
     # Dibujado de menues
-
     if estado_del_programa["menu_principal"]:
         for boton in lista_de_botones_menu_principal:
             dibujar_boton(boton)
@@ -104,4 +106,5 @@ while estado_del_programa['salir'] == False:
 
     #pygame.display.update()
     # musica_fondo.stop()  # Detiene la música al salir
+
 pygame.quit()
